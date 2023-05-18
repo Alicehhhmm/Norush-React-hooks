0de2a5998aee165e8119e3fb66896e5ec1c36659
Alicehhhmm <1245500390@qq.com>
2023-05-18 16:35:00 +0800

    fix: hook index

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import AHome from './AHome';
 import Button from './Button';
 import PrimaryButton from './PrimaryButton';
 import useBoolean from './useBoolean';
+import useToggle from './useToggle';
 import useMemoizedFn from './useMemoizedFn';
 import useSet from './useSet';
 import useSetState from './useSetState';
-import useToggle from './useToggle';
 
 export {
   AHome,
