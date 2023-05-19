b61bccb28cad46d226d4c59cdaa85c6b5ef4e329
Alicehhhmm <1245500390@qq.com>
2023-05-19 13:45:48 +0800

    fix: add .file

---
+++ .fatherrc.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'father';
+
+export default defineConfig({
+  // more father config: https://github.com/umijs/father/blob/master/docs/config.md
+  esm: {
+    output: 'dist',
+    ignores: [
+      'src/**/demo/**', // 避免打包demo文件到npm包里面
+    ],
+    // 打包的时候自动引入antd的样式链接
+    extraBabelPlugins: [
+      [
+        'babel-plugin-import',
+        {
+          libraryName: 'antd',
+          libraryDirectory: 'es',
+          style: true,
+        },
+      ],
+    ],
+  },
+});
